99babcdf7be983868fcfee26b56a5aafc483cbe6
kunalpurimkbk <kunalpurimkbk@gmail.com>
2018-11-20 20:57:32 +0530

    Add base code

---
+++ README.md
@@ -0,0 +1,1 @@
+"# stackoverflow_53385609" 
---
+++ target/classes/schema.sql
@@ -0,0 +1,4 @@
+create sequence hibernate_sequence start with 1 increment by 1
+create table about (id bigint not null, description varchar(255), image binary(255), name varchar(255), primary key (id))
+create table article (id bigint not null, description varchar(255), image binary(255), name varchar(255), primary key (id))
+create table menu_item (id bigint not null, description varchar(255), image binary(255), name varchar(255), primary key (id))
